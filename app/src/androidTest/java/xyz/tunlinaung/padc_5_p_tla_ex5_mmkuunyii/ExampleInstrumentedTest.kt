592756459fcca90f737a541a2809833f22c05050
tunlinaung <tunlinaung.java@gmail.com>
2018-08-08 20:59:53 +0630

    initial project setup

---
+++ app/src/androidTest/java/xyz/tunlinaung/padc_5_p_tla_ex5_mmkuunyii/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package xyz.tunlinaung.padc_5_p_tla_ex5_mmkuunyii
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("xyz.tunlinaung.padc_5_p_tla_ex5_mmkuunyii", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/xyz/tunlinaung/padc_5_p_tla_ex5_mmkuunyii/LoginActivity.kt
@@ -0,0 +1,12 @@
+package xyz.tunlinaung.padc_5_p_tla_ex5_mmkuunyii
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
